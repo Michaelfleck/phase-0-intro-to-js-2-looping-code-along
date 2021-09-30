75835e0f6550852c7779f9620f09e0b1d5bbeeb9
mfleck <michael.j.fleck@gmail.com>
2021-09-30 17:25:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const arrnames = [];
+
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+        arrnames[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+    return arrnames;
+}
+
+
+
+function countDown(num) {
+    while (num>= 0) {
+        console.log(num);
+        num--;
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
